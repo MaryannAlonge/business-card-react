eb62f4310b600c5773a12a96a3927c09ca2794a4
Jumoke Alonge <ariyikemaryann@gmail.com>
2023-01-07 12:08:14 +0100

    initial commit

---
+++ src/components/Info.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Info() {
+  return (
+    <div className='container'>
+      <div className="info"></div>
+      <h3 className='name'>Maryann Alonge</h3>
+      <h5 className='title'>Full Stack Developer/Engineer</h5>
+      <a className='web' href="https://www.linkedin.com/in/maryann-alonge-0946ba1a3/">maryannalonge.com</a>
+      <button className='btn'>Email</button>
+    </div>
+  )
+}
